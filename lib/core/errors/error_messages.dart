49d0f400e0f482291c35dbfd36572b9be9703bed
Yavuz Selim Erdoğan <yavuz.selimm.erdogan@gmail.com>
2026-02-23 22:27:50 +0300

    update local source functions

--- lib/core/errors/error_messages.dart
+++ lib/core/errors/error_messages.dart
@@ -6,4 +6,14 @@ class ErrorMessages {
 
   static const String noInternetCannotSend =
       'İnternet bağlantısı yok. Rapor gönderilemedi.';
+
+  static const String companyInsertError = 'Şirket eklenirken bir hata oluştu.';
+
+  static const String companyFetchError =
+      'Şirketler listelenirken bir hata oluştu.';
+
+  static const String companyUpdateError =
+      'Şirket güncellenirken bir hata oluştu.';
+
+  static const String companyDeleteError = 'Şirket silinirken bir hata oluştu.';
 }
